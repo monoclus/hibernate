ae7b6e2c4a0e10199e420355063daa28419ecc15
Ibin Issac <ibin.issac@impezar.com>
2022-02-12 21:38:26 +0530

    Hibernate Junit Changes

--- src/test/java/at/alley/hibernate_work/entities/Book_DBUnit_Test.java
+++ src/test/java/at/alley/hibernate_work/entities/Book_DBUnit_Test.java
@@ -1,35 +1,59 @@
 package at.alley.hibernate_work.entities;
 
-import org.junit.jupiter.api.Test;
+import com.github.database.rider.core.DBUnitRule;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.util.EntityManagerProvider;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-import java.util.Objects;
-
+import static com.github.database.rider.core.util.EntityManagerProvider.clear;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class Book_DBUnit_Test {
+/**
+ * @author ibinissac
+ * @createdOn 12/02/22
+ * @email services@impezar.com
+ * @company Impezar Private Limited Kochi India
+ */
 
-    private EntityManager getEntityManager()
-    {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("BooksDbConnection");
-        return Objects.requireNonNull(entityManagerFactory.createEntityManager());
-    }
+@RunWith(JUnit4.class)
+public class Book_DBUnit_Test {
+    @Rule
+    public EntityManagerProvider emProvider = EntityManagerProvider.instance("booksDB");
+
+    @Rule
+    public DBUnitRule dbUnitRule = DBUnitRule.instance(emProvider.connection());
+
 
     @Test
+    @DataSet(value="books.yml")
     public void save_BookEntity_And_QueryTheRecord_SameEM() {
+        BookEntity firstBook = (BookEntity) clear("booksDB").
+                em().createQuery("select b from BookEntity b  where b.id = 1").
+                getSingleResult();
+        BookEntity secondBook = (BookEntity) clear("booksDB").
+                em().createQuery("select b from BookEntity b  where b.id = 2").
+                getSingleResult();
+        BookEntity thirdBook = (BookEntity) clear("booksDB").
+                em().createQuery("select b from BookEntity b  where b.id = 3").
+                getSingleResult();
+        assertThat(firstBook).isNotNull();
+        assertThat(firstBook.getId()).isEqualTo(1l);
+        assertThat(firstBook.getTitle()).isEqualTo("Mama Mia");
+        assertThat(firstBook.getLeadtext()).isEqualTo("Mama Mia Long text");
 
-        // TODO: Populate the database with DBUnit
-        //       Source: src/test/resources/datasets/books.yml
+        assertThat(secondBook).isNotNull();
+        assertThat(secondBook.getId()).isEqualTo(2l);
+        assertThat(secondBook.getTitle()).isEqualTo("Esofagus");
+        assertThat(secondBook.getLeadtext()).isEqualTo("Esofagus Long text");
 
-        EntityManager entityManager = getEntityManager();
-        BookEntity rec_2 = entityManager.find(BookEntity.class, 2L);
-        assertThat(rec_2).isNotNull();
-        assertThat(rec_2.getTitle()).isEqualTo("Esofagus");
-        assertThat(rec_2.getId()).isEqualTo(2L);
 
+        assertThat(thirdBook).isNotNull();
+        assertThat(thirdBook.getId()).isEqualTo(3l);
+        assertThat(thirdBook.getTitle()).isEqualTo("Manduras");
+        assertThat(thirdBook.getLeadtext()).isEqualTo("Manduras Long text");
     }
 
 }
