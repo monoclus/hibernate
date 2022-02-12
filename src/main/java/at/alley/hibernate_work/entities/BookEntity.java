b2e61e08769df69d60d2093962048e00c4c6b279
Relu <relu.botinant@gmail.com>
2022-02-12 23:56:03 +0100

    Tests with data

--- src/main/java/at/alley/hibernate_work/entities/BookEntity.java
+++ src/main/java/at/alley/hibernate_work/entities/BookEntity.java
@@ -2,13 +2,14 @@ package at.alley.hibernate_work.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class BookEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
---
+++ src/test/java/at/alley/hibernate_work/entities/Book_Data_Test.java
@@ -0,0 +1,52 @@
+package at.alley.hibernate_work.entities;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Book_Data_Test {
+
+    EntityManager entityManager;
+
+    @BeforeEach
+    public void creataEntityManager() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("BooksDbConnection");
+        assertThat(entityManagerFactory).isNotNull();
+        this.entityManager = entityManagerFactory.createEntityManager();
+        assertThat(entityManager).isNotNull();
+    }
+
+    private Long createBook() {
+            entityManager.getTransaction().begin();
+            BookEntity oldBookEntity = new BookEntity("Book 3", "Leadtext 3");
+            entityManager.persist(oldBookEntity);
+            entityManager.getTransaction().commit();
+            return oldBookEntity.getId();
+    }
+
+    @Test
+    public void create_Book_And_Reload_It() {
+
+        Long bookId = createBook();
+        entityManager.clear();
+
+        entityManager.getTransaction().begin();
+        BookEntity newBookEntity = entityManager.find(BookEntity.class, bookId);
+        assertThat(newBookEntity).isNotNull();
+        assertThat(newBookEntity.getId()).isEqualTo(bookId);
+        entityManager.getTransaction().commit();
+
+    }
+
+    @Test
+    public void test_JPQL() {
+        createBook();
+
+        Query query = entityManager.createQuery("select count(b) from BookEntity b");
+        Long count = (Long)query.getSingleResult();
+        assertThat(count).isGreaterThan(0);
+    }
+}
