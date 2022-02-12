3b13a4694f9c5bb6fd37457ee38dcea8aef52321
fcapar <ferat.capar@booking.com>
2022-02-12 15:06:18 +0100

    Test data loader books_dbunit_test implementation

--- src/test/java/at/alley/hibernate_work/entities/Book_DBUnit_Test.java
+++ src/test/java/at/alley/hibernate_work/entities/Book_DBUnit_Test.java
@@ -1,35 +1,39 @@
 package at.alley.hibernate_work.entities;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-import java.util.Objects;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class Book_DBUnit_Test {
 
-    private EntityManager getEntityManager()
-    {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("BooksDbConnection");
-        return Objects.requireNonNull(entityManagerFactory.createEntityManager());
+    @Mock
+    private EntityManager entityManager;
+
+    @BeforeEach
+    void setUp() {
+        Map<Long, BookEntity> booksDB = new TestDataLoader().loadData("datasets/books.yml");
+        when(entityManager.find(eq(BookEntity.class), anyLong())).
+                then(invocation ->
+                        booksDB.get(invocation.getArguments()[1]));
     }
 
     @Test
     public void save_BookEntity_And_QueryTheRecord_SameEM() {
-
-        // TODO: Populate the database with DBUnit
-        //       Source: src/test/resources/datasets/books.yml
-
-        EntityManager entityManager = getEntityManager();
         BookEntity rec_2 = entityManager.find(BookEntity.class, 2L);
         assertThat(rec_2).isNotNull();
         assertThat(rec_2.getTitle()).isEqualTo("Esofagus");
         assertThat(rec_2.getId()).isEqualTo(2L);
-
     }
 
 }
